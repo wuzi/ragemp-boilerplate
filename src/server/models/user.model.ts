df51b83ad01bdf28e30c64a7002ccaa160d57314
Lucas Godoy <lcsdegodoy@outlook.com>
2019-08-16 09:53:21 -0300

    feat(model): add user model

---
+++ src/server/models/user.model.ts
@@ -0,0 +1,23 @@
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+
+export interface User extends mongoose.Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+}, {
+  timestamps: true,
+});
+
+UserSchema.pre('save', function (this: User): void {
+  this.password = bcrypt.hashSync(this.password, 10);
+});
+
+const User = mongoose.model<User>('User', UserSchema);
+export default User;
