7ca8da39035016eec2e78713c82ac169b51f698c
Lucas Godoy <lcsdegodoy@outlook.com>
2021-09-02 10:44:01 -0300

    refactor: move dotenv to inside bootstrap func

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,9 +3,10 @@ import databaseLoader from './loaders/database.loader';
 import languageLoader from './loaders/lang.loader';
 import bootstrapLoader from './loaders/bootstrap.loader';
 import moduleLoader from './loaders/module.loader';
-dotenv.config();
 
 async function bootstrap(): Promise<void> {
+  dotenv.config();
+
   await databaseLoader();
 
   await Promise.all([
